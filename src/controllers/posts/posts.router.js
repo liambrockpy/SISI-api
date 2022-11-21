12cfc4bac7b1c6b4b5c94c87ae30a95e37e54271
Guillaume <guillaume.ws.rame@gmail.com>
2022-11-21 10:08:58 +0000

    This will add the routers

--- src/controllers/posts/posts.router.js
+++ src/controllers/posts/posts.router.js
@@ -7,8 +7,55 @@ postsRouter.get('/', async (req, res) => {
         const posts = await postsService.findAll()
         res.status(200).send(posts)
     } catch (err) {
-        res.status(500).send(err.message)
+        res.status(500).send(err.message);
     }
 })
 
-module.exports = postsRouter
+// Get posts by id
+
+postsRouter.get('/:id', (req, res) => {
+    try {
+        const requestPost = req.params.postID;
+        const selectedPost = postsService.find(requestPost);
+        if (!selectedPost) {
+            throw new Error("There is no post with that ID")
+        }
+        else {
+            res.send(selectedPost);
+        }
+    }
+        catch (error) {
+            res.status(404).send({message: error.message});
+        }
+    }
+)
+
+
+// Post a new post
+
+postsRouter.post("/", (req, res) => {
+    try {
+        const postData = req.posts
+        const newPost = postsService.create(postData)
+        res.status(201).send(`${newPost} has been created`)
+    } catch (error) {
+        res.status(404).send({message: error.message});
+    }
+})
+
+// POST COMMENT ON POST 
+// /PostID/COMMENTS
+
+postsRouter.post("/:id/comment", (req, res) => {
+    try {
+        const comData = req.comments
+        const newComment = postsService.createComment(comData)
+        res.status(201).send(`${newComment} has been created`)
+    }
+    catch (error) {
+        res.status(404).send({message: error.message});
+    }
+})
+
+
+module.exports = postsRouter;
