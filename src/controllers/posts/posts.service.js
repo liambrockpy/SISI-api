2bba6cf6104200ba8250d82a1fbc8213a34535ec
“liambrockpy” <liambrock72@gmail.com>
2022-11-21 10:08:54 +0000

    Create service methods

--- src/controllers/posts/posts.service.js
+++ src/controllers/posts/posts.service.js
@@ -1,10 +1,44 @@
 const postsData = require('../../../db/posts.json')
+const uid = require('uid')
+const dayjs = require('dayjs')
 
 const findAll = async () => new Promise((res, rej) => {
     const posts = postsData.posts
     res(posts)
 })
 
+const find = async (id) => new Promise((res, rej) => {
+    const selectedPost = postsData.posts.filter(post => post.id === id)[0]
+    res(selectedPost)
+})
+
+const create = async (post) => new Promise((res, rej) => {
+    const postId = uid()
+    const dateNow = dayjs()
+
+    const newPost = {
+        ...post,
+        postId,
+        dateNow,
+        comments: null,
+        emoji: null
+    }
+
+    postsData.posts.push(newPost)
+    res(newPost)
+})
+
+const createComment = async (id, comment) => new Promise(async (res, rej) => {
+    const selectedPost = await find(id)
+    selectedPost.comments.push(comment)
+    const selectedIndex = postsData.posts.findIndex(post => post.postId === id)
+    postsData.posts.splice(selectedIndex, 1, selectedPost)
+    res(selectedPost)
+})
+
 module.exports = {
-    findAll
+    findAll,
+    find,
+    create,
+    createComment
 }
