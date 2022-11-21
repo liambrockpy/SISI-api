6a2566abc758a9a9327cb3b0917cf8fc20da5774
“liambrockpy” <liambrock72@gmail.com>
2022-11-21 16:08:00 +0000

    Add visiting page for app

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 const postsRouter = require('./controllers/posts/posts.router');
 const bodyParser = require('body-parser')
 
-const port = 3000;
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 
 // parse JSON into JS
 app.use(bodyParser.json())
 
-// test
-app.get('/api', (req, res) => res.status(200).send('Hello World!'));
+// app page
+app.get('/', (req, res) => res.send('public/index.html'));
 
 app.use('/api/posts', postsRouter)
 
